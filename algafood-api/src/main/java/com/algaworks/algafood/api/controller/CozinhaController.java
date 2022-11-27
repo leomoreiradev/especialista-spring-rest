94dfb9517c385f9a5db95b7335c385ef3a061ec7
Leandro Moreira <ilegra.oliveira@agi.com.br>
2022-11-27 18:13:51 -0300

    Cria controllers

---
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        return cozinhaRepository.listar();
+    }
+
+    @GetMapping(value = "/{cozinhaId}")
+    public Cozinha buscar(@PathVariable Long cozinhaId) {
+        return cozinhaRepository.porId(cozinhaId);
+    }
+}
